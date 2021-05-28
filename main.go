a221cf8adf3143d78f48627389b39a40684725ec
Yuri <chudinovyuri@gmail.com>
2021-05-28 18:41:07 +0500

    Work in progress

--- main.go
+++ main.go
@@ -5,16 +5,34 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main()  {
+	// cat text.txt | go run main.go
 	scanner := bufio.NewScanner(os.Stdin)
 
+	bufSize := 1024 * 1024 * 5 // 5 Mb
+	buf := make([]byte, 0, bufSize)
+	scanner.Buffer(buf, bufSize)
+
+	i:=0
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		text := scanner.Text()
+
+		for _, line := range strings.Split(text, "\n") {
+
+			if line == "" {
+				continue
+			}
+
+			fmt.Println(i, line)
+
+			i++
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		log.Println(err)
+		log.Printf("%v", err)
 	}
 }
